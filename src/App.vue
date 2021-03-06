ebc4b733515826b687cd6a7cc511ca9a13d433a8
Brozzor <46285168+Brozzor@users.noreply.github.com>
2021-03-06 20:06:58 +0100

    mise en forme de la page login

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style>
+@import './assets/css/styles.css';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -17,16 +14,8 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+body {
+  background: #0c1624 !important;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/views/Login.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="appCapsule" class="pt-0">
+    <div class="login-form mt-1">
+      <div class="section">
+        <img
+          src="img/cocktail.svg"
+          alt="image"
+          class="form-image"
+        />
+      </div>
+      <div class="section mt-1 text-white" >
+        <h1 style="font-size: 34px;font-weight: 700;">Bartender</h1>
+        <h4 style="font-size: 15px;font-weight: 500;">Rentrez le code de la soirée et commandez-vous à boire</h4>
+      </div>
+      <div class="section mt-1 mb-5">
+        <form action="app-pages.html">
+          <div class="form-group boxed">
+            <div class="input-wrapper">
+              <input
+                type="text"
+                class="form-control mt-3"
+                placeholder="Votre prénom"
+                maxlength="20"
+              />
+              <i class="clear-input">
+              </i>
+            </div>
+          </div>
+
+          <div class="form-group boxed">
+            <div class="input-wrapper">
+              <input
+                type="number"
+                class="form-control mt-2"
+                placeholder="Code de la soirée"
+                max="9999"
+                min="1"
+              />
+              <i class="clear-input">
+              </i>
+            </div>
+          </div>
+
+          <div class="form-button-group" style="background: #0f1c2d;">
+            <button type="submit" class="btn btn-primary btn-block btn-lg">
+              C'est partit !
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Login",
+};
+</script>
