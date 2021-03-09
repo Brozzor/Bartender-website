0aacfd69e440bda21692c79461ae0362c2ee0011
bertyn <bertynboulikou@gmail.com>
2021-03-10 00:04:02 +0100

    ajout des mutation de cocktail

--- src/store/index.js
+++ src/store/index.js
@@ -5,9 +5,16 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    cocktails: [],
 
   },
   mutations: {
+    addCocktail({ state }, cocktail) {
+      state.cocktails.push(cocktail)
+    },
+    removeCocktail({ state }, idCocktail) {
+      state.cocktails.splice(idCocktail, 1)
+    }
   },
   actions: {
   },
