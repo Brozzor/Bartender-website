87a3f1401f6bf70cef2f4025fce315ecbb7140ce
Brozzor <46285168+Brozzor@users.noreply.github.com>
2021-03-18 01:16:33 +0100

    mise en place des router-link sur la navbar admin

--- src/components/BottomMenu.vue
+++ src/components/BottomMenu.vue
@@ -1,31 +1,56 @@
 <template>
-    <div class="appBottomMenu" style="background: #0F1C2F;border-top-color: #1B283B;">
-        <a href="index.html" class="item">
-            <div class="col">
-                <img class="md hydrated iconbottom" height="26" src="/img/icons/control.svg" alt="">
-            </div>
-        </a>
-        <a href="app-components.html" class="item">
-            <div class="col">
-                <img class="md hydrated iconbottom" height="26" src="/img/icons/cocktail.svg" alt="">
-            </div>
-        </a>
-        <a href="page-chat.html" class="item">
-            <div class="col">
-                <img class="md hydrated iconbottom" height="26" src="/img/icons/bottle.svg" alt="">
-            </div>
-        </a>
+  <div
+    class="appBottomMenu"
+    style="background: #0f1c2f; border-top-color: #1b283b"
+  >
+    <router-link to="configuration" class="item">
+      <div class="col">
+        <img
+          class="md hydrated iconbottom"
+          height="26"
+          src="/img/icons/control.svg"
+          alt=""
+        />
+      </div>
+    </router-link>
 
-        <a href="javascript:;" class="item" data-toggle="modal" data-target="#sidebarPanel">
-            <div class="col">
-                 <img class="md hydrated iconbottom" height="26" src="/img/icons/log.svg" alt="">
-            </div>
-        </a>
-    </div>
+    <router-link to="cocktail" class="item">
+      <div class="col">
+        <img
+          class="md hydrated iconbottom"
+          height="26"
+          src="/img/icons/cocktail.svg"
+          alt=""
+        />
+      </div>
+    </router-link>
+
+    <router-link to="consommable" class="item">
+      <div class="col">
+        <img
+          class="md hydrated iconbottom"
+          height="26"
+          src="/img/icons/bottle.svg"
+          alt=""
+        />
+      </div>
+    </router-link>
+
+    <router-link to="log" class="item">
+      <div class="col">
+        <img
+          class="md hydrated iconbottom"
+          height="26"
+          src="/img/icons/log.svg"
+          alt=""
+        />
+      </div>
+    </router-link>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "BottomMenu"
-}
+  name: "BottomMenu",
+};
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,9 @@ import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 import Dashboard from '../views/admin/Dashboard.vue'
 import Configuration from '../views/admin/Configuration.vue'
+import Cocktail from '../views/admin/Cocktail.vue'
+import Consommable from '../views/admin/Consommable.vue'
+import Log from '../views/admin/Log.vue'
 import ErrorView from '../views/Layout/error.vue'
 
 Vue.use(VueRouter)
@@ -23,6 +26,21 @@ const routes = [
     path: '/admin/configuration',
     name: 'Configuration',
     component: Configuration
+  },
+  {
+    path: '/admin/cocktail',
+    name: 'Cocktail',
+    component: Cocktail
+  },
+  {
+    path: '/admin/consommable',
+    name: 'Consommable',
+    component: Consommable
+  },
+  {
+    path: '/admin/log',
+    name: 'Log',
+    component: Log
   },
   {
     path: '/admin',
