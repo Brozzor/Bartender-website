007a5787e4e3bfc30ab83c7b5bf31335276a1cf6
Bromain <377roro@gmail.com>
2024-09-12 23:01:09 +0200

    correction d'un bug empechant le build

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,6 @@
 module.exports = {
     pwa: {
       workboxPluginMode: 'GenerateSW',
-      workboxOptions: {
-        exclude: [/service-worker\.js$/],  // Exclure les fichiers déjà nommés service-worker.js
-      },
       iconPaths: {
         favicon32: 'favicon-32x32.png',
         favicon16: 'favicon-16x16.png',
